d6bf0971c7a06390c98133a3bdc06530d61eaeed
jirkakoutny <koutny.jiri@gmail.com>
2017-08-02 13:49:25 +0200

    User part

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const path = require('path');
+require('./config/config');
+
+const _ = require('lodash');
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
+
+var { mongoose } = require('./db/mongoose');
+var { User } = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 const publicPath = path.join(__dirname, '/public');
 const port = process.env.PORT || 3000;
@@ -7,5 +16,45 @@ const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
+app.use(bodyParser.json());
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
 
 app.listen(port, () => { console.log(`Server is up on ${port}`) });
+
+module.exports = { app };
