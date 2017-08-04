27591ad9519f7afaf344d5d5a8eac6119981415c
jirkakoutny <koutny.jiri@gmail.com>
2017-08-04 13:47:11 +0200

    Sanitize private key for Firebase

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -4,7 +4,7 @@ console.log(`Initializing Firebase on ${process.env.FIREBASE_DB_URL}`);
 
 admin.initializeApp({
     credential: admin.credential.cert({
-        "private_key": process.env.FIREBASE_PRIVATE_KEY,
+        "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         "client_email": process.env.FIREBASE_CLIENT_EMAIL,
     }),
     databaseURL: process.env.FIREBASE_DB_URL
